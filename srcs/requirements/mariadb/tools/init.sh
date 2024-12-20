c81bcbbda3aacf8a1d3a17ba897bd24f180aff59
Seongjko <akth101@icloud.com>
2024-12-20 10:32:05 +0900

    Fix: added mariadb root pass

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -9,6 +9,7 @@ if [ ! -f "$INIT_FLAG" ]; then
     mariadb -e "CREATE DATABASE ${DB_NAME} DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;"
     mariadb -e "CREATE USER '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';"
     mariadb -e "GRANT ALL ON ${DB_NAME}.* TO '${DB_USER}'@'%';"
+    mariadb -e "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('${ROOT_PASSWORD}');"
     mariadb -e "FLUSH PRIVILEGES;"
     touch "$INIT_FLAG"
     service mariadb stop
