93d452dc1035494ac484248a6107c570eaae10c6
Seongjko <akth101@icloud.com>
2024-12-20 12:30:24 +0900

    fix: mariadb root passwd setting

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -9,7 +9,7 @@ if [ ! -f "$INIT_FLAG" ]; then
     mariadb -e "CREATE DATABASE ${DB_NAME} DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;"
     mariadb -e "CREATE USER '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';"
     mariadb -e "GRANT ALL ON ${DB_NAME}.* TO '${DB_USER}'@'%';"
-    mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${ROOT_PASSWORD}';"
+    mariadb -e "ALTER USER root@localhost IDENTIFIED VIA mysql_native_password USING PASSWORD('${ROOT_PASSWORD}');" 
     mariadb -e "FLUSH PRIVILEGES;"
     touch "$INIT_FLAG"
     service mariadb stop
