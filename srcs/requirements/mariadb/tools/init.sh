506d0d442c12cdd8113f62b9e614d6fc50039846
Seongjko <akth101@icloud.com>
2024-12-20 09:58:58 +0900

    Fix: erased /dev/null

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -6,11 +6,6 @@ INIT_FLAG="/var/lib/mysql/initialization_done.flag"
 
 if [ ! -f "$INIT_FLAG" ]; then
     service mariadb start
-
-    until mysqladmin ping >/dev/null 2>&1; do
-        echo "Waiting for MariaDB to start..."
-        sleep 1
-    done
     mariadb -e "CREATE DATABASE ${DB_NAME} DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;"
     mariadb -e "CREATE USER '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';"
     mariadb -e "GRANT ALL ON ${DB_NAME}.* TO '${DB_USER}'@'%';"
